47fd02227f77d17f2b1ddb93930474079fe40941
Fabian Küppers <f.kueppers@hak-kitz.ac.at>
2017-11-10 12:12:32 +0100

    Starting with the Login Form

--- StudioManager/StudioManager/Form1.Designer.cs
+++ StudioManager/StudioManager/Form1.Designer.cs
@@ -28,20 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(116, 225);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_username
+            // 
+            this.txt_username.Location = new System.Drawing.Point(116, 70);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(100, 20);
+            this.txt_username.TabIndex = 1;
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(116, 97);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(100, 20);
+            this.txt_password.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(476, 370);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.TextBox txt_password;
     }
 }
 
--- StudioManager/StudioManager/Form1.cs
+++ StudioManager/StudioManager/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace StudioManager
 {
@@ -15,6 +16,25 @@ namespace StudioManager
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection con = new MySqlConnection("server=sql11.freemysqlhosting.net; username=sql11204165; password=uNEp4qPhYs; database=sql11204165;");
+            MySqlCommand cmd = new MySqlCommand("SELECT COUNT (*) AS cnt FROM Login WHERE username=@usr AND password=@pwd");
+            cmd.Parameters.Clear();
+            cmd.Parameters.AddWithValue("@usr", txt_username.Text);
+            cmd.Parameters.AddWithValue("@pwd", txt_password.Text);
+
+            con.Open();
+            MessageBox.Show(con.State.ToString());
+            MessageBox.Show(cmd.ExecuteScalar().ToString());
+            con.Close();
         }
     }
 }
