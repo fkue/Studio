f4d63ac4fe49497484b08cba1b7bfaccbf2d6046
Fabian Küppers <f.kueppers@hak-kitz.ac.at>
2017-11-17 09:15:22 +0100

    Changes to ConnectionString

--- StudioManager/StudioManager/Form1.cs
+++ StudioManager/StudioManager/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
+using System.Collections.Specialized;
 
 namespace StudioManager
 {
@@ -26,7 +27,7 @@ namespace StudioManager
         private void button1_Click(object sender, EventArgs e)
         {
             //MySqlConnection con = new MySqlConnection("server=sql11.freemysqlhosting.net; username=sql11204165; password=uNEp4qPhYs; database=sql11204165;");
-            MySqlConnection con = Properties.Settings.Default.Context.
+            MySqlConnection con = System.Configuration.ConfigurationManager.ConnectionStrings["StudioDBConnection"].ConnectionString;
             //MySqlCommand cmd = new MySqlCommand("SELECT * FROM Login WHERE username='"+txt_username.Text+"' AND password='"+ txt_password.Text + "'", con);
             MySqlCommand cmd = new MySqlCommand("SELECT count(*) as cnt from Login where username = @usr and password = @pwd", con);
             cmd.Parameters.Clear();
